0d1d4e1f3bcdb9af61d574016dd268a6953ef509
ndivyakrish <43902514+ndivyakrish@users.noreply.github.com>
2018-10-06 14:26:17 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -3,21 +3,21 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        int n;
+        int a;
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
-        n = s.nextInt();
-        if(n > 0)
+        System.out.print();
+        a= s.nextInt();
+        if(a> 0)
         {
-            System.out.println("The given number "+n+" is Positive");
+            System.out.println(" Positive");
         }
-        else if(n < 0)
+        else if(a < 0)
         {
-            System.out.println("The given number "+n+" is Negative");
+            System.out.println(" Negative");
         }
         else
         {
-            System.out.println("The given number "+n+" is neither Positive nor Negative ");
+            System.out.println("");
         }
     }
 }
